1a6b22ef28affd386b499f0d9651f914f0240568
Даниил Уткин <dan4ezzz@gmail.com>
2023-02-01 20:16:57 +0300

    Add Categories

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Header} from './components/ui/Header';
-import {Preloader} from './components/ui/Preloader';
 import {Footer} from './components/ui/Footer';
 import {Home} from './pages/Home';
 import {NotFound} from './pages/NotFound';
@@ -13,7 +12,7 @@ function App() {
         <main className="container content">
           <Routes>
             <Route path="/" element={<Home/>}></Route>
-            <Route path="/category/:id" element={<Category/>}></Route>
+            <Route path="/category/:name" element={<Category/>}></Route>
             <Route path="*" element={<NotFound/>}></Route>
           </Routes>
         </main>
